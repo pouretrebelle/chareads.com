d3d89e53d89961a2aeaefd1bbdac30f74155b877
Charlotte Dann <ask@pouretrebelle.com>
2020-02-09 19:04:06 +0000

    :heavy_minus_sign: Remove unused dependencies and gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,17 +8,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-typescript`,
       options: {
-        isTSX: false,
-        jsxPragma: `React`,
         allExtensions: true,
-        allowNamespaces: false,
       },
     },
     {
       resolve: 'gatsby-plugin-root-import',
-      options: {
-        root: path.join(__dirname, 'src'),
-      },
     },
     {
       resolve: 'gatsby-source-filesystem',
@@ -43,48 +37,10 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
+        plugins: [`gatsby-remark-smartypants`],
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     //trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
-    // `gatsby-plugin-feed`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `Gatsby Starter Blog`,
-    //     short_name: `GatsbyJS`,
-    //     start_url: `/`,
-    //     background_color: `#ffffff`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `content/assets/gatsby-icon.png`,
-    //   },
-    // },
-    // `gatsby-plugin-react-helmet`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
