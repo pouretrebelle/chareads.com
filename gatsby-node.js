7f354ef8717050bf19145e4fe7914e446477f1db
Charlotte Dann <ask@pouretrebelle.com>
2020-02-09 20:27:35 +0000

    :fire: Flatten gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,14 @@
 require('ts-node').register()
+const path = require('path')
 
-exports.createPagesStatefully = require('./src/lib/gatsby-node/createPagesStatefully').createPagesStatefully
+exports.createPagesStatefully = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const activity = reporter.activityTimer(`createPage /test`)
+  activity.start()
+  createPage({
+    path: '/test',
+    component: path.resolve('./src/Test.tsx'),
+  })
+  activity.end()
+}
