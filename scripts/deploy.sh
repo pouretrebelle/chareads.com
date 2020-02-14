d9f20bdc3e72d310d63a1e0c71e5cd4a222f38df
Charlotte Dann <ask@pouretrebelle.com>
2020-02-14 16:14:20 +0000

    :rocket: Add deployment script and run on CircleCI

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cross-env \
+NODE_PATH=$NODE_PATH:./src \
+  gatsby build --prefix-paths  && \
+gh-pages \
+  -d public \
+  -r https://$GITHUB_ACCESS_TOKEN@github.com/pouretrebelle/chareads.com.git \
+  -u "Charlotte Dann <hi@cha.rs>" \
+  -m "Deploy [ci skip]"
