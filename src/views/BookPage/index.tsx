e97c4a01f43e97b64e1efd28d7dfb3fe918f0493
Charlotte Dann <ask@pouretrebelle.com>
2020-02-15 09:02:00 +0000

    :sparkles: Show video mention data on BookPage

--- src/views/BookPage/index.tsx
+++ src/views/BookPage/index.tsx
@@ -1,20 +1,37 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
 
 import { RawBook, Book } from 'types/book'
-import { normalizeItem } from 'utils/graphql/normalize'
+import { RawVideoSnapshot, VideoSnapshot } from 'types/video/snapshot'
+import { normalizeItem, normalizeArray } from 'utils/graphql/normalize'
 import Layout from 'Layout'
 import H from 'components/H'
 
 interface Props {
   data: {
     bookData: RawBook
+    timestampMentionData: {
+      edges: {
+        node: RawVideoSnapshot
+      }[]
+    }
+    featuredVideoData: {
+      edges: {
+        node: RawVideoSnapshot
+      }[]
+    }
   }
 }
 
-const BookPage: React.FC<Props> = ({ data: { bookData } }) => {
+const BookPage: React.FC<Props> = ({
+  data: { bookData, timestampMentionData, featuredVideoData },
+}) => {
   const book = normalizeItem(bookData) as Book
+  const timestampMentions = normalizeArray(
+    timestampMentionData
+  ) as VideoSnapshot[]
+  const featuredVideos = normalizeArray(featuredVideoData) as VideoSnapshot[]
 
   return (
     <Layout>
@@ -31,6 +48,52 @@ const BookPage: React.FC<Props> = ({ data: { bookData } }) => {
           fluid={book.image.childImageSharp.fluid}
         />
       </figure>
+
+      {featuredVideos.length > 0 && (
+        <>
+          <H as="h3" size="M">
+            Featured video{featuredVideos.length > 1 && 's'}
+          </H>
+          <ol>
+            {featuredVideos.map(({ title, slug, image }) => (
+              <li key={slug}>
+                <Link to={slug}>
+                  {title}
+                  <figure style={{ maxWidth: 200, margin: 0 }}>
+                    <Img
+                      key={image.childImageSharp.fluid.src}
+                      fluid={image.childImageSharp.fluid}
+                    />
+                  </figure>
+                </Link>
+              </li>
+            ))}
+          </ol>
+        </>
+      )}
+
+      {timestampMentions.length > 0 && (
+        <>
+          <H as="h3" size="M">
+            Mentions in {featuredVideos.length > 0 && 'other '}videos
+          </H>
+          <ol>
+            {timestampMentions.map(({ title, slug, image }) => (
+              <li key={slug}>
+                <Link to={slug}>
+                  {title}
+                  <figure style={{ maxWidth: 200, margin: 0 }}>
+                    <Img
+                      key={image.childImageSharp.fluid.src}
+                      fluid={image.childImageSharp.fluid}
+                    />
+                  </figure>
+                </Link>
+              </li>
+            ))}
+          </ol>
+        </>
+      )}
     </Layout>
   )
 }
@@ -39,6 +102,22 @@ export const query = graphql`
   query BookPage($id: String!) {
     bookData: markdownRemark(id: { eq: $id }) {
       ...BookFields
+    }
+    timestampMentionData: allVideos(
+      filter: { timestamps: { elemMatch: { book: { id: { eq: $id } } } } }
+    ) {
+      edges {
+        node {
+          ...VideoSnapshotFields
+        }
+      }
+    }
+    featuredVideoData: allVideos(filter: { ownedBy: { id: { eq: $id } } }) {
+      edges {
+        node {
+          ...VideoSnapshotFields
+        }
+      }
     }
   }
 `
