456c9f2b5f80b184423e5f0cfe9cc54ec3421f09
Charlotte Dann <ask@pouretrebelle.com>
2020-02-16 11:05:26 +0000

    :sparkles: Scrape basic video data from YouTube video data

---
+++ scripts/migration/videos.ts
@@ -0,0 +1,26 @@
+// data derived from https://developers.google.com/youtube/v3/docs/search/list?apix=true&apix_params=%7B%22part%22%3A%22snippet%22%2C%22forMine%22%3Atrue%2C%22maxResults%22%3A50%2C%22order%22%3A%22date%22%2C%22type%22%3A%22video%22%7D
+import youtubeData from './data/youtube'
+import { YoutubeVideo } from './youtube/types'
+
+import {
+  getTitle,
+  getOwnedBy,
+  getQuote,
+  getDescription,
+  getImage,
+} from './youtube/utils'
+
+const output = youtubeData.map((video: YoutubeVideo, i) => {
+  const data = {
+    title: getTitle(video),
+    ownedBy: getOwnedBy(video),
+    youtubeId: video.id.videoId,
+    datePublished: video.snippet.publishedAt,
+    image: getImage(video),
+    quote: getQuote(video),
+    description: getDescription(video),
+    timestamps: [],
+  }
+
+  return data
+})
