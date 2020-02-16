69f73c36fff39448b5bba87e84a75e009ec64dce
Charlotte Dann <ask@pouretrebelle.com>
2020-02-16 11:40:11 +0000

    :sparkles: Get folder name for videos

--- scripts/migration/youtube/utils.ts
+++ scripts/migration/youtube/utils.ts
@@ -1,5 +1,6 @@
 import { YoutubeVideo, MarkdownTimestamp } from './types'
 import { unformatTimestamp } from '../../../src/utils/formatting/time'
+import slugify from 'slugify'
 
 const getBookFromString = (string: string): string => {
   const matches = string.match(/([^&|]+) by (.+)/)
@@ -73,4 +74,20 @@ export const getTimestamps = (video: YoutubeVideo): MarkdownTimestamp[] => {
   })
 
   return timestamps
+}
+
+const wasteWords = ['by', 'the', 'in', 'on', 'at', 'to', 'a', 'is', 'and']
+
+export const getFolder = (video: YoutubeVideo): string => {
+  const publishDate = video.snippet.publishedAt.slice(0, 10)
+  const title = getTitle(video)
+    .toLowerCase()
+    .split(' ')
+    .filter((w) => !wasteWords.includes(w))
+    .slice(0, 6)
+    .join(' ')
+  return slugify(`${publishDate} ${title}`, {
+    lower: true,
+    remove: /[^A-Za-z0-9_\- ]/g,
+  })
 }
