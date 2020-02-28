4e4370ad5694aba50892d0312d35005a38d7b641
Charlotte Dann <ask@pouretrebelle.com>
2020-02-28 17:06:39 +0000

    :rocket: Add getViewCounts step to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,9 @@
 
 cross-env \
 NODE_PATH=$NODE_PATH:./src \
+  ts-node scripts/stats/getViewCounts.ts && \
+cross-env \
+NODE_PATH=$NODE_PATH:./src \
   gatsby build --prefix-paths  && \
 gh-pages \
   -d public \
