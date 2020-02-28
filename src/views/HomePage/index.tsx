91b68c78166b67bfd1232631b54b19c034be3865
Charlotte Dann <ask@pouretrebelle.com>
2020-02-28 21:19:16 +0000

    :fire: Only show books on the HomePage

--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -7,13 +7,11 @@ import { normalizeArray } from 'utils/graphql/normalize'
 import PATHS from 'routes/paths'
 import Layout from 'Layout'
 import { RawBookCard, BookCardType } from 'types/book/card'
-import { RawVideoCard, VideoCardType } from 'types/video/card'
 import H from 'components/H'
 import Wrapper from 'components/Wrapper'
 import Grid from 'components/Grid'
 import GridItem from 'components/Grid/GridItem'
 import BookCard from 'components/cards/BookCard'
-import VideoCard from 'components/cards/VideoCard'
 import { screenMin } from 'styles/responsive'
 import { toVW, getWidthOfColumns } from 'styles/layout'
 
@@ -34,20 +32,11 @@ interface Props extends PageProps {
         node: RawBookCard
       }[]
     }
-    videoData: {
-      edges: {
-        node: RawVideoCard
-      }[]
-    }
   }
 }
 
-const HomePage: React.FC<Props> = ({
-  data: { bookData, videoData },
-  location,
-}) => {
+const HomePage: React.FC<Props> = ({ data: { bookData }, location }) => {
   const books = normalizeArray(bookData) as BookCardType[]
-  const videos = normalizeArray(videoData) as VideoCardType[]
 
   return (
     <Layout location={location}>
@@ -56,7 +45,7 @@ const HomePage: React.FC<Props> = ({
           <Link to={PATHS.HOME}>Chareads</Link>
         </H>
         <StyledIntro>
-          <p>
+          <p style={{ marginBottom: '2em' }}>
             Hello, I&rsquo;m Charlotte and I love to read anything and
             everything. Chareads is where I record and review every book I read.
             Have a poke around and find your next favourite.
@@ -66,28 +55,14 @@ const HomePage: React.FC<Props> = ({
 
       <Wrapper>
         <H as="h2" size="L" decorative>
-          <Link to={PATHS.BOOKS}>Books</Link>
+          <Link to={PATHS.BOOKS}>Recent reads</Link>
         </H>
       </Wrapper>
       <Grid as="ol">
         {books.length &&
           books.map((book) => (
             <GridItem as="li" key={book.id} span={1} spanFromM={3}>
-              <BookCard book={book} />
-            </GridItem>
-          ))}
-      </Grid>
-
-      <Wrapper>
-        <H as="h2" size="L" decorative>
-          <Link to={PATHS.VIDEOS}>Videos</Link>
-        </H>
-      </Wrapper>
-      <Grid as="ol">
-        {videos.length &&
-          videos.map((video) => (
-            <GridItem as="li" key={video.id} span={1} spanFromM={3}>
-              <VideoCard video={video} />
+              <BookCard book={book} big={book.rating7 >= 6} />
             </GridItem>
           ))}
       </Grid>
@@ -100,21 +75,11 @@ export const query = graphql`
     bookData: allMarkdownRemark(
       sort: { fields: frontmatter___dateRated, order: DESC }
       filter: { frontmatter: { rating7: { ne: null } } }
-      limit: 8
+      limit: 12
     ) {
       edges {
         node {
           ...BookCardFields
-        }
-      }
-    }
-    videoData: allVideos(
-      sort: { fields: datePublished, order: DESC }
-      limit: 8
-    ) {
-      edges {
-        node {
-          ...VideoCardFields
         }
       }
     }
