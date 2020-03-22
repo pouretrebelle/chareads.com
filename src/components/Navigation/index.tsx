6e8c1ab5e3d4d48a92e22f648f0ae46ef85e2114
Charlotte Dann <ask@pouretrebelle.com>
2020-03-22 21:51:50 +0000

    :lipstick: Update navigation styling

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,58 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import { PageProps } from 'types/page'
 import Wrapper from 'components/Wrapper'
 import { COLOR } from 'styles/tokens'
-import { screen } from 'styles/responsive'
-import { GAP, toPerc } from 'styles/layout'
+import { screenMax } from 'styles/responsive'
 
 import NavLink from './NavLink'
 
-const StyledNav = styled.nav`
-  background: ${COLOR.BACKGROUND_LIGHT};
-  box-shadow: 0 -2px 5px 2px rgba(0, 0, 0, 0.05), 0 -2px 10px 2px rgba(0,0,0,0.05);
-  margin-bottom: 2em;
+interface OpenProps {
+  isOpen?: boolean
+}
 
-  ${screen.s`
-    margin-bottom: ${toPerc(GAP.S)};
+const StyledWrapper = styled.div`
+  position: fixed;
+  z-index: 80;
+  top: 0;
+  left: 0;
+  right: 0;
+`
+
+const StyledMenuButton = styled.button<OpenProps>`
+  position: absolute;
+  display: block;
+  z-index: 100;
+  top: 0.5em;
+  right: -0.5em;
+  width: 2.5em;
+  height: 2.5em;
+
+  > * {
+    position: absolute;
+    width: 1.5em;
+    height: 0.12em;
+    background: black;
+    border-radius: 2px;
+    top: 50%;
+    left: 0.5em;
+    transition: opacity 0.2s linear, transform 0.2s linear;
+    transform-origin: 50% 50%;
+    margin-top: -0.06em;
+  }
+
+  > *:nth-child(1) {
+    transform: translate(0, -0.5em);
+    ${({ isOpen }): string =>
+      isOpen && `transform: rotate(-45deg) scale(1.1, 1);`}
+  }
+
+  > *:nth-child(2) {
+    ${({ isOpen }): string => isOpen && `opacity: 0;`}
+  }
+
+  > *:nth-child(3) {
+    transform: translate(0, 0.5em);
+    ${({ isOpen }): string =>
+      isOpen && `transform: rotate(45deg) scale(1.1, 1);`}
+  }
+`
+
+const StyledNav = styled.nav<OpenProps>`
+  background: ${COLOR.BACKGROUND};
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  transition: opacity 0.2s ease-in;
+  padding-right: 1.5em;
+
+  ${screenMax.s`
+    height: 100vh;
+    overflow: auto;
   `}
 
-  ${screen.m`
-    margin-bottom: ${toPerc(GAP.M)};
-  `}
-
-  ${screen.l`
-    margin-bottom: ${toPerc(GAP.L)};
-  `}
-
-  ${screen.xl`
-    margin-bottom: ${toPerc(GAP.XL)};
+  ${({ isOpen }): string =>
+    !isOpen &&
+    `
+    opacity: 0;
+    pointer-events: none;
   `}
 `
 
-const Navigation: React.FC<PageProps> = ({ location }) => (
-  <StyledNav>
-    <Wrapper>
-      <NavLink to="/" activeMatches={[/^\/$/]} pathname={location.pathname}>
-        Home
-      </NavLink>
-      <NavLink
-        to="/books"
-        activeMatches={[/^\/book[s/]/]}
-        pathname={location.pathname}
-      >
-        Book reviews
-      </NavLink>
-      <NavLink
-        to="/videos"
-        activeMatches={[/^\/video[s/]/]}
-        pathname={location.pathname}
-      >
-        Videos
-      </NavLink>
-    </Wrapper>
-  </StyledNav>
-)
+const Navigation: React.FC<PageProps> = ({ location }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <StyledWrapper>
+      <Wrapper style={{ position: 'relative' }}>
+        <StyledMenuButton
+          onClick={(): void => setIsOpen(!isOpen)}
+          isOpen={isOpen}
+        >
+          <span />
+          <span />
+          <span />
+        </StyledMenuButton>
+      </Wrapper>
+
+      <StyledNav isOpen={isOpen}>
+        <Wrapper>
+          <NavLink to="/" activeMatches={[/^\/$/]} pathname={location.pathname}>
+            Home
+          </NavLink>
+          <NavLink
+            to="/books"
+            activeMatches={[/^\/book[s/]/]}
+            pathname={location.pathname}
+          >
+            Book reviews
+          </NavLink>
+          <NavLink
+            to="/videos"
+            activeMatches={[/^\/video[s/]/]}
+            pathname={location.pathname}
+          >
+            Videos
+          </NavLink>
+        </Wrapper>
+      </StyledNav>
+    </StyledWrapper>
+  )
+}
 
 export default Navigation
