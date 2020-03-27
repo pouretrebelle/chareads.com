b18b7955f58dc587ea348f6d0cedb698c8264d41
Charlotte Dann <ask@pouretrebelle.com>
2020-03-27 14:01:59 +0000

    :art: Clean up onCreateNode file

--- src/lib/gatsby-node/onCreateNode.ts
+++ src/lib/gatsby-node/onCreateNode.ts
@@ -1,54 +1,13 @@
-import { createContentDigest } from 'gatsby-core-utils'
-
-import { getBookSlug, getVideoSlug } from 'utils/urls/slugs'
-import { getAffiliateLinks } from 'utils/urls/affiliates'
-import { makeIsbn10, makeIsbn13 } from 'utils/formatting/isbn'
-import { getVideoViewCount } from './utils/addToNode'
+import { createBookNode, createVideoNode } from './utils/createNodes'
 
 export const onCreateNode = ({ node, actions }): void => {
   const { createNode } = actions
 
   if (node.internal.type === 'MarkdownRemark') {
     if (node.fileAbsolutePath.match(/content\/books\//)) {
-      const { frontmatter, ...content } = node
-
-      const isbn10 = makeIsbn10(
-        node.frontmatter.isbn13 || node.frontmatter.isbn10
-      )
-      const isbn13 = makeIsbn13(
-        node.frontmatter.isbn10 || node.frontmatter.isbn13
-      )
-
-      createNode({
-        ...frontmatter,
-        ...content,
-        slug: getBookSlug(node.frontmatter),
-        isbn10,
-        isbn13,
-        links: getAffiliateLinks(isbn13 || isbn10),
-
-        id: `${node.id}-book`,
-        internal: {
-          type: 'Book',
-          content: node.rawMarkdownBody,
-          contentDigest: createContentDigest(node),
-        },
-      })
+      createBookNode({ node, createNode })
     } else if (node.fileAbsolutePath.match(/content\/videos\//)) {
-      const { frontmatter, ...content } = node
-      createNode({
-        ...frontmatter,
-        ...content,
-        slug: getVideoSlug(node.frontmatter),
-        viewCount: getVideoViewCount(node.frontmatter),
-
-        id: `${node.id}-video`,
-        internal: {
-          type: 'Video',
-          content: node.rawMarkdownBody,
-          contentDigest: createContentDigest(node),
-        },
-      })
+      createVideoNode({ node, createNode })
     }
   }
 }
