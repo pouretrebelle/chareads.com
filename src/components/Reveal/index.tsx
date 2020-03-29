aabdbfbb1825133e21b2544624934eb542f2f5dc
Charlotte Dann <ask@pouretrebelle.com>
2020-03-29 17:39:11 +0100

    :lipstick: Add animation to book summary reveal

---
+++ src/components/Reveal/index.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, createRef } from 'react'
+import styled from 'styled-components'
+
+const TRANSITION_DURATION_IN_MS = 200
+
+interface ContentProps {
+  ref: React.RefObject<React.ReactNode>
+  isOpen: boolean
+  isTransitioning: boolean
+  contentHeight: number
+}
+
+const StyledContent = styled.div<ContentProps>`
+  margin: 0;
+  will-change: opacity, max-height;
+  transition: opacity ${TRANSITION_DURATION_IN_MS}ms ease-in-out, max-height ${TRANSITION_DURATION_IN_MS}ms ease-in-out;
+  overflow: hidden;
+
+  ${({ isOpen, isTransitioning }): string =>
+    !isOpen &&
+    !isTransitioning &&
+    `
+      position: absolute;
+      opacity: 0;
+      pointer-events: none;
+  `}
+
+  ${({ isTransitioning }): string =>
+    isTransitioning &&
+    `
+      opacity: 0;
+      max-height: 0;
+  `}
+
+  ${({ isOpen, contentHeight }): string =>
+    isOpen &&
+    `
+      opacity: 1;
+      max-height: ${contentHeight}px;
+  `}
+`
+
+interface Props {
+  children: React.ReactNode
+  open: boolean
+  ariaId: string
+}
+
+const Reveal: React.FC<Props> = ({ children, open, ariaId }) => {
+  const [isOpen, setIsOpen] = useState(open)
+  const [isTransitioning, setIsTransitioning] = useState(false)
+  const [contentHeight, setContentHeight] = useState(null)
+  const contentEl = createRef<HTMLDivElement>()
+
+  useEffect(() => {
+    if (open) {
+      const ref = contentEl.current as HTMLElement
+      setContentHeight(ref.clientHeight)
+      setIsTransitioning(true)
+    } else {
+      setIsOpen(false)
+      setTimeout(() => setIsTransitioning(false), TRANSITION_DURATION_IN_MS)
+    }
+  }, [open])
+
+  useEffect(() => {
+    if (isTransitioning && !isOpen) setTimeout(() => setIsOpen(true), 50)
+  }, [isTransitioning])
+
+  return (
+    <StyledContent
+      ref={contentEl}
+      isOpen={isOpen}
+      isTransitioning={isTransitioning}
+      contentHeight={contentHeight}
+      id={ariaId}
+      aria-hidden={!isOpen}
+    >
+      {children}
+    </StyledContent>
+  )
+}
+
+export default Reveal
--- src/views/BookPage/BookReview.tsx
+++ src/views/BookPage/BookReview.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import { Book } from 'types/book'
+import Reveal from 'components/Reveal'
+import ArrowIcon from 'components/icons/ArrowIcon'
+import { FONT } from 'styles/tokens'
+
+const SUMMARY_ARIA_ID = 'book-summary'
+
+const StyledSummaryTrigger = styled.button`
+  font-size: ${FONT.SIZE.S};
+  font-weight: ${FONT.WEIGHT.BOLD};
+`
+
+interface ArrowProps {
+  open: boolean
+}
+
+const StyledArrowIcon = styled(ArrowIcon)<ArrowProps>`
+  transition: transform 0.2s linear;
+  transform: rotate(${({ open }): number => (open ? 270 : 90)}deg);
+  margin: 0 0.5em 0 0;
+`
+
+const StyledSummary = styled.div`
+  font-size: ${FONT.SIZE.S};
+`
 
 const StyledBookReview = styled.div`
   margin-top: 1em;
@@ -9,17 +33,26 @@ const StyledBookReview = styled.div`
 
 type Props = Pick<Book, 'summary' | 'html'>
 
-const BookReview: React.FC<Props> = ({ summary, html }) => (
-  <StyledBookReview>
-    {summary && (
-      <details style={{ fontSize: '0.75em' }}>
-        <summary>Book sumary</summary>
-        {summary}
-      </details>
-    )}
+const BookReview: React.FC<Props> = ({ summary, html }) => {
+  const [isSummaryOpen, setisSummaryOpen] = useState(false)
 
-    {html && <div dangerouslySetInnerHTML={{ __html: html }} />}
-  </StyledBookReview>
-)
+  return (
+    <StyledBookReview>
+      {html && <div dangerouslySetInnerHTML={{ __html: html }} />}
+
+      <StyledSummaryTrigger
+        onClick={(): void => setisSummaryOpen(!isSummaryOpen)}
+        aria-expanded={isSummaryOpen}
+        aria-controls={SUMMARY_ARIA_ID}
+      >
+        <StyledArrowIcon open={isSummaryOpen} />
+        Show book summary
+      </StyledSummaryTrigger>
+      <Reveal open={isSummaryOpen} ariaId={SUMMARY_ARIA_ID}>
+        <StyledSummary>{summary}</StyledSummary>
+      </Reveal>
+    </StyledBookReview>
+  )
+}
 
 export default BookReview
