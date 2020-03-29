b392ed54cc2b591be1794fba418e935f5a0682cb
Charlotte Dann <ask@pouretrebelle.com>
2020-03-29 15:31:20 +0100

    :lipstick: Add book opening animation to BookCard

--- src/components/cards/BookCard/index.tsx
+++ src/components/cards/BookCard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { SimpleInterpolation } from 'styled-components'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 
@@ -7,8 +7,15 @@ import { BookCardType } from 'types/book'
 import StarRating from 'components/StarRating'
 import ArrowIcon from 'components/icons/ArrowIcon'
 import { FONT, COLOR, BORDER_RADIUS, BREAKPOINT } from 'styles/tokens'
+import { screenMin } from 'styles/responsive'
 
-const StyledBookCard = styled(Link)`
+interface BookCardProps {
+  big: boolean
+}
+
+const StyledBookCard = styled(
+  ({ big, ...props }) => <Link {...props} /> // eslint-disable-line
+)<BookCardProps>`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
@@ -19,14 +26,43 @@ const StyledBookCard = styled(Link)`
   overflow: hidden;
   background: ${COLOR.BACKGROUND_DARK};
   border-radius: ${BORDER_RADIUS.S};
+  outline: 0 !important;
+  --perspective: 800px;
+  --book-pages-color: #afbdbb;
+
+  ${({ big }): SimpleInterpolation =>
+    big &&
+    screenMin.m`
+    --perspective: 1600px;
+  `};
 `
 
-const StyledImg = styled(Img)`
-  box-shadow: 0 0.2em 0.5em rgba(0, 0, 0, 0.1), 0 0 0.3em rgba(0, 0, 0, 0.05);
+const StyledImgWrapper = styled.figure`
   max-width: 100%;
   height: auto;
   margin: 0.5em;
+  background: var(--book-pages-color);
+  box-shadow: 0 0.2em 0.5em rgba(0, 0, 0, 0.1);
   transform: scale(var(--book-scale));
+`
+
+const StyledImg = styled(Img)`
+  vertical-align: bottom;
+  transition: transform 0.3s ease-out;
+  transform-origin: 0 50%;
+  box-shadow: 0 0 0.3em rgba(0, 0, 0, 0.05);
+  transform: perspective(var(--perspective)) rotateY(0);
+
+  a:hover &,
+  a:focus & {
+    transition: transform 0.15s ease-out;
+    transform: perspective(var(--perspective)) rotateY(-30deg);
+  }
+
+  a:active & {
+    transition: transform 0.5s ease-out;
+    transform: perspective(var(--perspective)) rotateY(-80deg);
+  }
 `
 
 const StyledMeta = styled.div`
@@ -62,6 +98,7 @@ const BookCard: React.FC<Props> = ({ book, featured, big, hideDetails }) => {
   return (
     <StyledBookCard
       to={book.slug}
+      big={big}
       style={
         {
           background: hideDetails
@@ -69,30 +106,31 @@ const BookCard: React.FC<Props> = ({ book, featured, big, hideDetails }) => {
             : featured && book.image.colors.lightMuted,
           color: book.image.colors.darkVibrant,
           '--primary-color': book.image.colors.vibrant,
+          '--book-pages-color': featured && book.image.colors.muted,
+          '--book-scale': ((book.bookHeight || 198) / 220).toFixed(2),
         } as object
       }
       title={`${book.title} by ${book.author}`}
     >
-      <StyledImg
-        fixed={
-          big
-            ? [
-                {
-                  ...book.image.childImageSharp.h150,
-                  media: `(max-width: ${BREAKPOINT.S - 1}px)`,
-                },
-                {
-                  ...book.image.childImageSharp.h350,
-                  media: `(min-width: ${BREAKPOINT.S}px)`,
-                },
-              ]
-            : book.image.childImageSharp.h150
-        }
-        style={{
-          '--book-scale': ((book.bookHeight || 198) / 220).toFixed(2),
-        }}
-        backgroundColor={book.image.colors.muted}
-      />
+      <StyledImgWrapper>
+        <StyledImg
+          fixed={
+            big
+              ? [
+                  {
+                    ...book.image.childImageSharp.h150,
+                    media: `(max-width: ${BREAKPOINT.S - 1}px)`,
+                  },
+                  {
+                    ...book.image.childImageSharp.h350,
+                    media: `(min-width: ${BREAKPOINT.S}px)`,
+                  },
+                ]
+              : book.image.childImageSharp.h150
+          }
+          backgroundColor={book.image.colors.muted}
+        />
+      </StyledImgWrapper>
       {!hideDetails && (book.rating7 || book.video) && (
         <StyledMeta>
           {book.video && (
