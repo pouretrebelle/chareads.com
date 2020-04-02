56010deb9b5f93ff968203d6115a6acc6ac94686
Charlotte Dann <hi@cha.rs>
2020-04-02 14:01:29 +0100

    :sparkles: Hide BookMeta under Reveal on mobile

--- src/views/BookPage/BookMeta.tsx
+++ src/views/BookPage/BookMeta.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import { Book } from 'types/book'
 import { shortFormatDate, formatYear } from 'utils/formatting/time'
-import { FONT } from 'styles/tokens'
+import useWindowSize from 'utils/hooks/useWindowSize'
+import { FONT, BREAKPOINT } from 'styles/tokens'
+import { trim } from 'styles/helpers'
+import { screen, screenMin } from 'styles/responsive'
+import Reveal from 'components/Reveal'
+import RevealTriggerWithArrow from 'components/Reveal/RevealTriggerWithArrow'
 import TagsList from './TagsList'
+
+const META_ARIA_ID = 'book-meta'
+
+const StyledRevealTrigger = styled(RevealTriggerWithArrow)`
+  ${screenMin.m`
+    display: none;
+  `}
+`
+
+const StyledReveal = styled.dl`
+  ${trim}
+
+  ${screen.s`
+    padding: 0.5em 0 0 1em;
+  `}
+`
 
 const StyledDt = styled.dt`
   font-size: ${FONT.SIZE.S};
@@ -13,7 +34,11 @@ const StyledDt = styled.dt`
 
 const StyledDd = styled.dd`
   font-size: ${FONT.SIZE.S};
-  margin: 0 0 1em;
+  margin: 0 0 0.5em;
+
+  ${screenMin.m`
+    margin: 0 0 1em;
+  `}
 `
 
 type Props = Pick<
@@ -35,54 +60,69 @@ const BookMeta: React.FC<Props> = ({
   readDates,
   dateRated,
   dateReviewed,
-}) => (
-  <div>
-    <dl>
-      {pageCount && (
-        <>
-          <StyledDt>Page count</StyledDt>
-          <StyledDd>{pageCount}</StyledDd>
-        </>
-      )}
-      {dateBookPublished && (
-        <>
-          <StyledDt>Year published</StyledDt>
-          <StyledDd>{formatYear(dateBookPublished)}</StyledDd>
-        </>
-      )}
-      {publisher && (
-        <>
-          <StyledDt>Publisher</StyledDt>
-          <StyledDd>{publisher}</StyledDd>
-        </>
-      )}
+}) => {
+  const { width: windowWidth } = useWindowSize()
+  const [isOpen, setIsOpen] = useState(false)
 
-      <TagsList tags={tags} />
-    </dl>
+  return (
+    <>
+      <StyledRevealTrigger
+        onClick={(): void => setIsOpen(!isOpen)}
+        open={isOpen}
+        ariaId={META_ARIA_ID}
+      >
+        Show book information
+      </StyledRevealTrigger>
 
-    <dl>
-      {readDates.length > 0 && (
-        <>
-          <StyledDt>Date read</StyledDt>
-          <StyledDd>
-            {shortFormatDate(readDates[readDates.length - 1][1])}
-          </StyledDd>
-        </>
-      )}
-      {dateRated && (
-        <>
-          <StyledDt>Date rated</StyledDt>
-          <StyledDd>{shortFormatDate(dateRated)}</StyledDd>
-        </>
-      )}
-      {dateReviewed && (
-        <>
-          <StyledDt>Date reviewed</StyledDt>
-          <StyledDd>{shortFormatDate(dateReviewed)}</StyledDd>
-        </>
-      )}
-    </dl>
-  </div>
-)
+      <Reveal
+        as={StyledReveal}
+        open={(windowWidth && windowWidth > BREAKPOINT.S) || isOpen}
+        ariaId={META_ARIA_ID}
+      >
+        {pageCount && (
+          <>
+            <StyledDt>Page count</StyledDt>
+            <StyledDd>{pageCount}</StyledDd>
+          </>
+        )}
+        {dateBookPublished && (
+          <>
+            <StyledDt>Year published</StyledDt>
+            <StyledDd>{formatYear(dateBookPublished)}</StyledDd>
+          </>
+        )}
+        {publisher && (
+          <>
+            <StyledDt>Publisher</StyledDt>
+            <StyledDd>{publisher}</StyledDd>
+          </>
+        )}
+
+        <TagsList tags={tags} />
+
+        {readDates.length > 0 && (
+          <>
+            <StyledDt>Date read</StyledDt>
+            <StyledDd>
+              {shortFormatDate(readDates[readDates.length - 1][1])}
+            </StyledDd>
+          </>
+        )}
+        {dateRated && (
+          <>
+            <StyledDt>Date rated</StyledDt>
+            <StyledDd>{shortFormatDate(dateRated)}</StyledDd>
+          </>
+        )}
+        {dateReviewed && (
+          <>
+            <StyledDt>Date reviewed</StyledDt>
+            <StyledDd>{shortFormatDate(dateReviewed)}</StyledDd>
+          </>
+        )}
+      </Reveal>
+    </>
+  )
+}
 
 export default BookMeta
