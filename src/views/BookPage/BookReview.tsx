de020da748cef6c77a358bc92e5d36a103dec494
Charlotte Dann <hi@cha.rs>
2020-04-02 13:49:12 +0100

    :art: Extract RevealTriggerWithArrow from BookReview

--- src/views/BookPage/BookReview.tsx
+++ src/views/BookPage/BookReview.tsx
@@ -3,25 +3,10 @@ import styled from 'styled-components'
 
 import { Book } from 'types/book'
 import Reveal from 'components/Reveal'
-import ArrowIcon from 'components/icons/ArrowIcon'
+import RevealTriggerWithArrow from 'components/Reveal/RevealTriggerWithArrow'
 import { FONT } from 'styles/tokens'
 
 const SUMMARY_ARIA_ID = 'book-summary'
-
-const StyledSummaryTrigger = styled.button`
-  font-size: ${FONT.SIZE.S};
-  font-weight: ${FONT.WEIGHT.BOLD};
-`
-
-interface ArrowProps {
-  open: boolean
-}
-
-const StyledArrowIcon = styled(ArrowIcon)<ArrowProps>`
-  transition: transform 0.2s linear;
-  transform: rotate(${({ open }): number => (open ? 270 : 90)}deg);
-  margin: 0 0.5em 0 0;
-`
 
 const StyledSummary = styled.div`
   font-size: ${FONT.SIZE.S};
@@ -40,14 +25,14 @@ const BookReview: React.FC<Props> = ({ summary, html }) => {
     <StyledBookReview>
       {html && <div dangerouslySetInnerHTML={{ __html: html }} />}
 
-      <StyledSummaryTrigger
+      <RevealTriggerWithArrow
         onClick={(): void => setisSummaryOpen(!isSummaryOpen)}
-        aria-expanded={isSummaryOpen}
-        aria-controls={SUMMARY_ARIA_ID}
+        open={isSummaryOpen}
+        ariaId={SUMMARY_ARIA_ID}
       >
-        <StyledArrowIcon open={isSummaryOpen} />
         Show book summary
-      </StyledSummaryTrigger>
+      </RevealTriggerWithArrow>
+
       <Reveal open={isSummaryOpen} ariaId={SUMMARY_ARIA_ID}>
         <StyledSummary>{summary}</StyledSummary>
       </Reveal>
