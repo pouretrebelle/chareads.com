eb7198a21ec357e5dfc7f40b660c961a4859e60d
Charlotte Dann <hi@cha.rs>
2020-04-03 16:23:24 +0100

    :bug: Fix SSR Helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-extract-image-colors',
   ],
