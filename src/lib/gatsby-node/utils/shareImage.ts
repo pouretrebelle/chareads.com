75fef362cf4eefa2ef23728e2f9a4bf34e87c717
Charlotte Dann <hi@cha.rs>
2020-04-09 22:49:41 +0100

    :sparkles: Add shadow to book in share image

--- src/lib/gatsby-node/utils/shareImage.ts
+++ src/lib/gatsby-node/utils/shareImage.ts
@@ -13,6 +13,10 @@ export interface ShareBook {
 
 const OUTPUT_WIDTH = 1200
 const OUTPUT_HEIGHT = 630
+const SVG_BUFFER = 40
+const SHADOW_BLUR = 15
+const SHADOW_OFFSET = 6
+const SHADOW_OPACITY = 0.3
 
 export const createBookShareImage = async (
   { image, bookHeight }: ShareBook,
@@ -24,17 +28,43 @@ export const createBookShareImage = async (
   const coverHeight = Math.round(2.4 * bookHeight)
   const coverWidth = Math.round((coverHeight * width) / height)
 
-  const padV = Math.round((OUTPUT_HEIGHT - coverHeight) / 2)
-  const padH = Math.round((OUTPUT_WIDTH - coverWidth) / 2)
+  const blur = await sharp(
+    Buffer.from(`
+      <svg
+        width="${coverWidth + SVG_BUFFER * 2}"
+        height="${coverHeight + SVG_BUFFER * 2}"
+      >
+        <rect
+          width="${coverWidth}"
+          height="${coverHeight}"
+          x="${SVG_BUFFER}"
+          y="${SVG_BUFFER + SHADOW_OFFSET}"
+          fill="rgba(0, 0, 0, ${SHADOW_OPACITY})"
+        />
+      </svg>`)
+  )
+    .blur(SHADOW_BLUR)
+    .toBuffer()
 
-  await stream
-    .resize({ height: coverHeight, width: coverWidth })
-    .extend({
-      top: padV,
-      bottom: padV,
-      left: padH,
-      right: padH,
-      background: image.colors.muted,
+  const cover = await stream
+    .resize({
+      height: coverHeight,
+      width: coverWidth,
     })
+    .toBuffer()
+
+  await sharp({
+    create: {
+      width: OUTPUT_WIDTH,
+      height: OUTPUT_HEIGHT,
+      channels: 3,
+      background: image.colors.muted,
+    },
+  })
+    .composite([
+      { input: blur, blend: 'multiply' },
+      { input: cover, blend: 'over' },
+    ])
+    .jpeg()
     .toFile(imagePath)
 }
