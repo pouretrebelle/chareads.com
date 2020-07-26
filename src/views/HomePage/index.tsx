2a103d4e89a03fb0b66fddf465f32dabacb8e04e
Charlotte Dann <hi@cha.rs>
2020-07-26 20:57:58 +0100

    :sparkles: Add cover image hero to homepage

--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -1,29 +1,42 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import styled from 'styled-components'
 
-import { PageProps } from 'types/page'
 import { normalizeArray } from 'utils/graphql/normalize'
 import PATHS from 'routes/paths'
 import Layout from 'Layout'
+import { PageProps } from 'types/page'
 import { BookCardType } from 'types/book'
-import H from 'components/H'
+import { GatsbyImageSharpFluid } from 'types/image'
 import Grid from 'components/Grid'
 import GridItem from 'components/Grid/GridItem'
 import BookCard from 'components/cards/BookCard'
 import Wrapper from 'components/Wrapper'
 import { screen, screenMin } from 'styles/responsive'
+import { COLOR } from 'styles/tokens'
 
+import Hero from './Hero'
 import SectionLink from './SectionLink'
+
+const StyledMainWrapper = styled.div`
+  background: ${COLOR.BACKGROUND};
+  position: relative;
+  z-index: 90;
+`
 
 const StyledIntroGrid = styled(Grid)`
   && {
-    margin-top: 1em;
+    padding-top: 1em;
     margin-bottom: 1em;
 
     ${screenMin.m`
-      margin-top: 3.5em;
-      margin-bottom: 1em;
+      padding-top: 2em;
+      margin-bottom: 2em;
+    `}
+
+    ${screenMin.l`
+      padding-top: 3em;
+      margin-bottom: 3em;
     `}
   }
 `
@@ -49,52 +62,61 @@ interface Props extends PageProps {
         node: BookCardType
       }[]
     }
+    cover: GatsbyImageSharpFluid
   }
 }
 
-const HomePage: React.FC<Props> = ({ data: { bookData }, location }) => {
+const HomePage: React.FC<Props> = ({ data: { bookData, cover }, location }) => {
   const books = normalizeArray(bookData) as BookCardType[]
 
   return (
-    <Layout location={location} navOpenOnDesktop>
-      <StyledIntroGrid>
-        <GridItem span={2} spanFromM={12} spanFromL={9} spanFromXL={8}>
-          <H as="h1" size="XXL" decorative style={{ marginLeft: '-0.1ch' }}>
-            <Link to={PATHS.HOME}>Chareads</Link>
-          </H>
-          <p>
-            Hello, I&rsquo;m Charlotte and I love to read anything and
-            everything. Chareads is where I record and review every book I read.
-            Have a poke around and find your next favourite.
-          </p>
-        </GridItem>
-      </StyledIntroGrid>
+    <Layout location={location} navOpenOnDesktop navInverted>
+      <Hero cover={cover} />
 
-      <StyledBookGrid as="ol">
-        {books.length &&
-          books.map((book) => (
-            <GridItem
-              as="li"
-              key={book.id}
-              span={1}
-              spanFromM={4}
-              spanFromL={3}
-            >
-              <BookCard book={book} featured={book.rating7 >= 6} />
-            </GridItem>
-          ))}
-      </StyledBookGrid>
+      <StyledMainWrapper>
+        <StyledIntroGrid>
+          <GridItem span={2} spanFromM={12} spanFromL={9} spanFromXL={8}>
+            <p>
+              Hello, I&rsquo;m Charlotte and I love to read anything and
+              everything. Chareads is where I record and review every book I
+              read. Have&nbsp;a&nbsp;poke around and find your next favourite.
+            </p>
+          </GridItem>
+        </StyledIntroGrid>
 
-      <StyledLinkWrapper>
-        <SectionLink to={PATHS.BOOKS}>Find more book reviews</SectionLink>
-        <SectionLink to={PATHS.VIDEOS}>See all my bookish videos</SectionLink>
-      </StyledLinkWrapper>
+        <StyledBookGrid as="ol">
+          {books.length &&
+            books.map((book) => (
+              <GridItem
+                as="li"
+                key={book.id}
+                span={1}
+                spanFromM={4}
+                spanFromL={2}
+              >
+                <BookCard book={book} featured={book.rating7 >= 6} />
+              </GridItem>
+            ))}
+        </StyledBookGrid>
+
+        <StyledLinkWrapper>
+          <SectionLink to={PATHS.BOOKS}>Find more book reviews</SectionLink>
+          <SectionLink to={PATHS.VIDEOS}>See all my bookish videos</SectionLink>
+        </StyledLinkWrapper>
+      </StyledMainWrapper>
     </Layout>
   )
 }
 
 export const query = graphql`
   query HomePage {
+    cover: file(relativePath: { eq: "cover.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 2000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
     bookData: allBook(
       sort: { fields: dateRated, order: DESC }
       filter: { rating7: { ne: null } }
