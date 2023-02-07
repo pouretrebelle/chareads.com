2589773c827af30161f839d6e159937df2682803
Charlotte Dann <hi@cha.rs>
2023-02-07 10:59:08 +0000

    :wrench: Add local dependencies for plugins

--- plugins/gatsby-plugin-vibrant-image/gatsby-node.js
+++ plugins/gatsby-plugin-vibrant-image/gatsby-node.js
@@ -1,36 +1,35 @@
-'use strict'
+"use strict";
 
-const fs = require('fs')
+const fs = require('fs');
 
-const Vibrant = require('node-vibrant')
+const Vibrant = require('node-vibrant');
 
-const Color = require('color')
+const Color = require('color');
 
-const CoreUtils = require('gatsby-core-utils')
+const CoreUtils = require('gatsby-core-utils');
 
 const defaultOptions = {
   extensions: ['jpg', 'png'],
-  exclude: [],
-}
+  exclude: []
+};
 
-const getHex = (rgb) => {
+const getHex = rgb => {
   return Color({
     r: rgb[0],
     g: rgb[1],
-    b: rgb[2],
-  }).hex()
-}
+    b: rgb[2]
+  }).hex();
+};
 
-exports.onCreateNode = async ({ node, actions }, pluginOptions) => {
-  const options = Object.assign({}, { ...defaultOptions, ...pluginOptions })
+exports.onCreateNode = async ({
+  node,
+  actions
+}, pluginOptions) => {
+  const options = Object.assign({}, { ...defaultOptions,
+    ...pluginOptions
+  });
 
-  if (
-    options &&
-    options.extensions &&
-    options.exclude &&
-    options.extensions.indexOf(node.extension) !== -1 &&
-    options.exclude.indexOf(`${node.name}${node.ext}`) === -1
-  ) {
+  if (options && options.extensions && options.exclude && options.extensions.indexOf(node.extension) !== -1 && options.exclude.indexOf(`${node.name}${node.ext}`) === -1) {
     // Transform the new node here and create a new node or
     // create a new node field.
     await Vibrant.from(node.absolutePath).getPalette((err, palette) => {
@@ -40,21 +39,20 @@ exports.onCreateNode = async ({ node, actions }, pluginOptions) => {
         lightVibrant: getHex(palette.LightVibrant._rgb),
         muted: getHex(palette.Muted._rgb),
         darkMuted: getHex(palette.DarkMuted._rgb),
-        lightMuted: getHex(palette.LightMuted._rgb),
-      }
-      const imageColorsNode = {
-        ...colors,
+        lightMuted: getHex(palette.LightMuted._rgb)
+      };
+      const imageColorsNode = { ...colors,
         id: `${node.id}-colors`,
         internal: {
           type: 'ImageColors',
-          contentDigest: CoreUtils.createContentDigest(node),
-        },
-      }
-      actions.createNode(imageColorsNode)
+          contentDigest: CoreUtils.createContentDigest(node)
+        }
+      };
+      actions.createNode(imageColorsNode);
       actions.createParentChildLink({
         parent: node,
-        child: imageColorsNode,
-      })
-    })
+        child: imageColorsNode
+      });
+    });
   }
-}
+};
