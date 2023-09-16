7d7d62c231ec35432814d9ecbcac454b7e752143
Charlotte Dann <pouretrebelle@github.com>
2023-09-16 09:05:11 +0100

    :wrench: Improve diff for nightly deploys
    Every night we redeploy with gh-pages but since it's saved in git, the diffs
    are bloating the whole project. Thankfully on a redeploy where only the youtube
    stats have changed, only the page-data files are edited, but since they're
    minified it's quite a big diff. This prettifies the page data files during the
    build process, so the nightly builds will cause minimal diffs.

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -5,4 +5,6 @@ NODE_PATH=$NODE_PATH:./src \
   ts-node scripts/stats/getYouTubeStats.ts && \
 cross-env \
 NODE_PATH=$NODE_PATH:./src \
-  gatsby build --prefix-paths
+  gatsby build --prefix-paths && \
+cross-env \
+  prettier --write "public/page-data/**/page-data.json" --ignore-path "" --loglevel warn
